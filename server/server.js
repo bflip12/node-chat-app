06869c6d037e285c3dee8c73e46d39fdcc2b712b
bflip12 <bflip12@gmail.com>
2017-12-22 12:48:40 -0500

    add connect and disconnect even handlers

--- server/server.js
+++ server/server.js
@@ -1,15 +1,27 @@
 const express = require('express');
+const http = require('http');
 const path = require('path');
+const socketIO = require('socket.io');
 
-var app = express();
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-console.log(__dirname + '/../public');
-console.log(publicPath);
+io.on('connection', (socket) => {
+  console.log('new user connected');
 
-app.listen(port, () => {
+  socket.on('disconnect', () => {
+    console.log('user disconected')
+  });
+});
+
+// console.log(__dirname + '/../public');
+// console.log(publicPath);
+
+server.listen(port, () => {
   console.log(`server is up on port ${port}`)
 });
